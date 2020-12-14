e5a58e583707c7963dc5aa8f0d84de2c9a16beed
Ebere Richard Emelike <ebere.emelike@gmail.com>
2020-12-14 16:28:48 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chap01aMyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
